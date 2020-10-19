9f75dca428510ed5d0eab936496504fc27f1773f
yacine <yacine.yousfi@ens.uvsq.fr>
2020-10-19 20:29:36 +0200

    deuxieme modification

--- tp_1 cmpl_programmation/src/Fraction.java
+++ tp_1 cmpl_programmation/src/Fraction.java
@@ -1,4 +1,17 @@
 
 public class Fraction {
+	int i,j;
+	public Fraction(int i,int j) {
+		this.i=i;
+		this.j=j;
+		
+	}
+	
+	public void  tostring(int x,int y) {
+		String chaine = null;
+		chaine=""+x+"/"+y+"";
+		System.out.println(chaine) ;
+		
+	}
 
 }
--- tp_1 cmpl_programmation/src/main.java
+++ tp_1 cmpl_programmation/src/main.java
@@ -3,6 +3,9 @@ public class main {
 
 	public static void main(String[] args) {
 		System.out.println("hello world !!");
+		Fraction f=new Fraction(3,4);
+		f.tostring(3,4);
+		
 
 	}
 
